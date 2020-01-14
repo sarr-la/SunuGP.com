2b475e934b30b38e1410d00f7e1457553783b61f
sarr-la <sarrabdoulayeyakhine7@gmail.com>
2020-01-14 09:46:45 +0000

    ajouter

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -76,7 +76,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('mot de passe') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('mot de passe') is-invalid @enderror" name="mot de passe" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
